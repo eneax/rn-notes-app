8f7606e58a12439d73dfbeb72d1df1f7b688e2d4
eneax <19818937+eneax@users.noreply.github.com>
2021-08-29 13:42:41 +0200

    Save notes

--- src/context/NotesContext.js
+++ src/context/NotesContext.js
@@ -7,7 +7,8 @@ const notesReducer = (state, action) => {
         ...state,
         {
           id: Math.floor(Math.random() * 9999999),
-          title: `Note #${state.length + Math.floor(Math.random() * 9999999)}`,
+          title: action.payload.title,
+          content: action.payload.content,
         },
       ];
     case "delete_note":
@@ -18,7 +19,8 @@ const notesReducer = (state, action) => {
 };
 
 const addNote = (dispatch) => {
-  return () => dispatch({ type: "add_note" });
+  return (title, content) =>
+    dispatch({ type: "add_note", payload: { title, content } });
 };
 
 const deleteNote = (dispatch) => {
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
+import { Context as NotesContext } from "../context/NotesContext";
+
 const CreateScreen = () => {
   const [title, setTitle] = React.useState("");
   const [content, setContent] = React.useState("");
+  const { addNote } = React.useContext(NotesContext);
 
   return (
     <View style={styles.screenContainerStyles}>
@@ -21,7 +24,7 @@ const CreateScreen = () => {
         style={styles.inputStyles}
       />
 
-      <Button title="Save note" />
+      <Button title="Save note" onPress={() => addNote(title, content)} />
     </View>
   );
 };
