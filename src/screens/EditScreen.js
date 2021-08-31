dde10154b04f175b8bb309549f07b33d5c73ecc2
eneax <19818937+eneax@users.noreply.github.com>
2021-08-31 21:21:58 +0200

    Initialize Edit screen state from Context

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,14 +1,50 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TextInput, StyleSheet } from "react-native";
+
+import { Context as NotesContext } from "../context/NotesContext";
 
 const EditScreen = ({ route }) => {
   const { noteId } = route.params;
+  const { state } = React.useContext(NotesContext);
+  const noteDetails = state.find(({ id }) => id === noteId);
+
+  const [title, setTitle] = React.useState(noteDetails.title);
+  const [content, setContent] = React.useState(noteDetails.content);
 
   return (
-    <View>
-      <Text>Edit Note: {noteId}</Text>
+    <View style={styles.screenContainerStyles}>
+      <Text style={styles.labelStyles}>Edit Title:</Text>
+      <TextInput
+        value={title}
+        onChangeText={(newTitle) => setTitle(newTitle)}
+        style={styles.inputStyles}
+      />
+
+      <Text style={styles.labelStyles}>Edit Content:</Text>
+      <TextInput
+        value={content}
+        onChangeText={(newContent) => setContent(newContent)}
+        style={styles.inputStyles}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screenContainerStyles: {
+    padding: 15,
+  },
+  labelStyles: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  inputStyles: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+  },
+});
 
 export default EditScreen;
