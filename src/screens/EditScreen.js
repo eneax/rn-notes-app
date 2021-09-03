16576a48a946ec0baff7034cd9cb0cd0b9a67d8a
eneax <19818937+eneax@users.noreply.github.com>
2021-09-03 21:06:47 +0200

    Edit note and navigate back

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -3,16 +3,16 @@ import React from "react";
 import { Context as NotesContext } from "../context/NotesContext";
 import Form from "../components/Form";
 
-const EditScreen = ({ route }) => {
+const EditScreen = ({ navigation, route }) => {
   const { noteId } = route.params;
-  const { state } = React.useContext(NotesContext);
+  const { state, editNote } = React.useContext(NotesContext);
   const noteDetails = state.find(({ id }) => id === noteId);
 
   return (
     <Form
       initialValues={{ title: noteDetails.title, content: noteDetails.content }}
       onSubmit={(title, content) => {
-        console.log(title, content);
+        editNote(noteId, title, content, () => navigation.pop());
       }}
     />
   );
